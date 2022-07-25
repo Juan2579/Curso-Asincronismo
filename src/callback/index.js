ddeca8673355db59fbd32da9b3fc181f801d522d
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-07-25 15:40:34 -0500

    Initial commit with callback and xmlhttprequest exercises

---
+++ src/callback/index.js
@@ -0,0 +1,24 @@
+//Ejercicio 1
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2)
+}
+
+console.log(calc(2, 2, sum))
+
+//Ejercicio 2
+
+setTimeout(function() {
+    console.log("Hola JavaScript")
+}, 2000)
+
+//Ejercicio 3
+
+function greeting(name){
+    console.log(`Hola ${name}`)
+}
+
+setTimeout(greeting, 2000, "Juan")
